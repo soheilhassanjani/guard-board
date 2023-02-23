6a2b5fd79fa25f7a87b7a20daf294ecbae70500d
Soheil Hassanjani <soheilpcmir@gmail.com>
2023-02-23 16:18:13 +0330

    .

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 import AppLayout from "@com-layouts/AppLayout";
 
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline p-3">Hello world!</h1>;
+  return (
+    <h1 className="p-3 mt-10 text-xl font-bold text-center">
+      به سامانه تنظیم لوح نگهبانی خوش آمدید.
+    </h1>
+  );
 }
 
 Home.getLayout = function getLayout(page) {
